0d5f4ff4930f40902c6dd77a58678f8558f29ce8
shubhjagtap <jagtapshubhangi97@gmail.com>
2022-05-24 19:11:08 +0530

    first commit

---
+++ src/Primitive/Int.java
@@ -0,0 +1,6 @@
+package Primitive;
+
+public class Int {
+	
+
+}
---
+++ src/Primitive/Numerical.java
@@ -0,0 +1,13 @@
+package Primitive;
+
+public class Numerical {
+public static void main(String[] args) {
+	
+byte var1=10;
+System.out.println(var1);
+byte a=127;
+byte b=-128;
+System.out.println(b);
+ 
+}
+}
